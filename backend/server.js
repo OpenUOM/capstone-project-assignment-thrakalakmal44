0cea2a3bef6cee6dbfb7c13f422ab5ee66d5b488
Tharaka Lakmal <tharakalakmal44@eng.pdn.ac.lk>
2023-05-29 21:54:17 +0530

    edit student corrected 2

--- backend/server.js
+++ backend/server.js
@@ -129,7 +129,7 @@ app.post("/editStudent", async function (req, res) {
   console.log(
     "Request received to update Student. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
+  let data = await updateStudent(reqBody.name, reqBody.age, reqBody.hometown, reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
