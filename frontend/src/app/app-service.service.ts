d00d5a3dfa574d654bfedc9b5f9c3c410f81c9f8
Tharaka Lakmal <tharakalakmal44@eng.pdn.ac.lk>
2023-05-30 16:54:46 +0530

    Frontend_2

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -10,11 +10,7 @@ export class AppServiceService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    if(environment.production == false){
-      this.ROOT_URL = 'test'
-    }else{
-      this.ROOT_URL = 'http://localhost:8080'
-    }
+    this.ROOT_URL = 'http://localhost:8080'
   }
 
   initializeDB(){
