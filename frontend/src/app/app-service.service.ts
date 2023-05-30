f3c123ba72fa84bcb2156f2195e3d552af3269d1
Tharaka Lakmal <tharakalakmal44@eng.pdn.ac.lk>
2023-05-30 14:02:58 +0530

    Frontend_1

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -13,7 +13,7 @@ export class AppServiceService {
     if(environment.production == false){
       this.ROOT_URL = 'test'
     }else{
-      this.ROOT_URL = 'api'
+      this.ROOT_URL = 'http://localhost:8080'
     }
   }
 
